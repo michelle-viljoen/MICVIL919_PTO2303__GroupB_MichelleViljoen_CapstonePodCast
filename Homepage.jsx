72e49e33ea47b9703a384b6bbd7b24ee6cb09490
Michelle Viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2024-03-11 23:14:17 +0000

    correct date and genres displayed

--- Homepage.jsx
+++ Homepage.jsx
@@ -5,40 +5,23 @@ import PodcastCard  from '../components/PodcastCard'
 const Homepage = ({token}) => {
     let navigate = useNavigate
 
-    const  [podcastData, setPodcastData] = useState(null)
+    const  [podcastData, setPodcastData] = useState([])
 
-    //  const PodcastCard = ({podcastData}) => {
-        
-    //     return (
-    //         <div className='preview__display'>
-    //             <div className='preview__content'>
-    //         {podcastData.map((podcast) => (
-            
-    //             <button key={podcast.id}>
-    //                 <h2 className='preview__title'>{podcast.title}</h2>
-    //                 <img className='preview__image' src={podcast.image} width="150px"/>
-    //                 <h3 className='preview__description'>{podcast.description}</h3>
-    //                 <p className='preview__seasons'>Seasons: {podcast.seasons}</p>
-    //                 <p className='preview__genres'>{podcast.genres}</p>
-    //                 <p className='preview__updated'>Last updated: {podcast.updated}</p>
-    //             </button>  
-    //       ))}  
-    //       </div>
-    //       </div>
-    //     )
-    // }
-
- console.log(podcastData)
-    
     useEffect(() => {
         fetch('https://podcast-api.netlify.app/shows')
         .then(res => { 
-            return res.json()
+            if (!res.ok) {
+                throw new Error("Something is wrong")
+            }
+             return res.json()
         })
         .then(data => {
-            setPodcastData(data)
+               setPodcastData(data)
+             console.log(data)
+           
         })
     }, [])
+
 
     const podcasts = podcastData.map((item) => {
         
@@ -49,22 +32,20 @@ const Homepage = ({token}) => {
             />
         )
     })
-
+    
     function handleLogout() {
         sessionStorage.removeItem('token')
         navigate('/') 
     }
-    console.log(podcastData)
+   
     return (
         <div className='homepage'>
             <header className="home__header">
             <img src=".\src\images\3.png" width="100px" className="logoImage" />
            <h3 className='welcome'>Welcome back, {token.user.user_metadata.full_name}!</h3> 
            </header>
-          {/* {podcastData && <PodcastCard podcastData={podcastData} className='preview__list'/>} */}
-         
            <div className="podcastCard">
-            <p>{podcasts}</p>
+            {podcasts}
            </div>
             <button onClick={handleLogout}>Logout</button>
             </div>
--- PodcastCard.jsx
+++ PodcastCard.jsx
@@ -1,95 +1,20 @@
 
-//import React, {useEffect, useState} from 'react'
-//{podcastData}
-// export default function PodcastCard () {
-//     const  [podcastData, setPodcastData] = useState(null)
-    
-//     useEffect(() => {
-//         fetch('https://podcast-api.netlify.app/shows')
-//         .then(res => { 
-//             return res.json()
-//         })
-//         .then(data => {
-//             setPodcastData(data)
-//         })
-//     }, [])
-//     console.log(podcastData)
-
-//  const podcast = <div>
-//  {podcastData.map((podcast) => (
-//      <div key={podcast.id}>
-//          <h2>{podcast.title}</h2>
-//          <h3>{podcast.description}</h3>
-//          <p>{podcast.seasons}</p>
-//          <p>{podcast.image}</p>
-//          <p>{podcast.genres}</p>
-//          <p>{podcast.updated}</p>
-//      </div>  
-// ))}  
-// </div>
-//         return (
-//           <div> {podcast && <PodcastCard podcastData={podcast}/>} </div>  
-//         )
-//     }
-
-
-
-// function PodcastCard() {
-    
-//         const  [podcastData, setPodcastData] = useState(null)
-//         function Card(props) {
-//             return (
-//                 <div>
-//                           <h2>{props.title}</h2>
-//                           <h3>{props.description}</h3>
-//                           <p>{props.seasons}</p>
-//                           <p>{props.image}</p>
-//                           <p>{props.genres}</p>
-//                 /         <p>{props.updated}</p>
-//                       </div>  
-//             )
-//         }
-    
-//     useEffect(() => {
-//         fetch('https://podcast-api.netlify.app/shows')
-//         .then(res => { 
-//             return res.json()
-//         })
-//         .then(data => {
-//             setPodcastData(data)
-//         })
-//     }, [])
-
-//     const podcast = podcastData.map(item => {
-//         return (
-//             <Card
-//             key={item.id} {...item}/>
-//         )
-//     })
-//     return (
-//         <div>{podcast}</div>
-//     )
-// }
-
-// export default PodcastCard
-
+import React, {useEffect, useState} from 'react'
 
 export default function PodcastCard (props)  {
-   
-    // const updated = new Date(props.podcastData.updated)
-    // console.log(updated)
-    // const updatedDate = updated.toDateString()
-    // const genreTitle = {
-    //     1: "Personal Growth",
-    //     2: "True Crime and Investigative Journalism",
-    //     3: "History", 
-    //     4: "Comedy",
-    //     5: "Entertainment",
-    //     6: "Business",
-    //     7: "Fiction",
-    //     8: "News",
-    //     9: "Kids and Family"
-    // }
+
+    const genreMap = {
+        1: 'Personal Growth',
+        2: 'True Crime and Investigative Journalism',
+        3: 'History',
+        4: 'Comedy',
+        5: 'Entertainment',
+        6: 'Business',
+        7: 'Fiction',
+        8: 'News',
+        9: 'Kids and Family',
+    };
+
     return (
         <div className='preview__display'>
             <div className='preview__content'>
@@ -100,9 +25,9 @@ export default function PodcastCard (props)  {
                 <img className='preview__image' src={props.image} width="150px"/>
                 <h3 className='preview__description'>{props.description}</h3>
                 <p className='preview__seasons'>Seasons: {props.seasons}</p>
-                <p className='preview__genres'>{props.genres}</p>
-                {/* <p className='preview__updated'>Last updated: {new Date(props.updated).toDateString()}</p> */}
-                <p className='preview__updated'>Last updated: {props.updated}</p>
+                <p className='preview__genres'>{props.genres.map((genreId) => genreMap[genreId]).join(', ')}</p>
+                <p className='preview__updated'>Last updated: {new Date(props.updated).toDateString()}</p>
+               
             </button>  
       
       </div>
